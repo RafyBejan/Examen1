6f336eea777882a0591e5596844e6a5a878922e0
bejan <bejan@RAFAEL12>
2024-01-22 10:36:01 +0200

    initial commit

---
+++ Examen1/Examen1/Order.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Examen1.Order;
+
+namespace Examen1
+{
+    internal class Order
+    {
+        public string Customer { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+
+        public interface IPaymentSystem
+        {
+            void ProcessPayment(decimal amount);
+        }
+        public abstract class PaymentSystem
+        {
+            public abstract void ProcessPayment(decimal amount);
+        }
+        public abstract class  CardPayment:PaymentSystem
+        {
+            public override void ProcessPayment(decimal amount)
+            {
+                Console.WriteLine($"Plata de {amount:C} a fost efectuată cu cardul.");
+            }
+        }
+        public class CashPayment : PaymentSystem
+        {
+            public override void ProcessPayment(decimal amount)
+            {
+                Console.WriteLine($"Plata de {amount:C} a fost efectuată cu bani cash.");
+            }
+        }
+        public class OrderProcessor
+        {
+            public void FulfillOrder(Order order, IPaymentSystem paymentSystem)
+            {
+                Console.WriteLine($"Comanda pentru {order.Customer} din data de {order.Date.ToShortDateString()} cu valoarea totală de {order.Total:C} va fi procesată.");
+
+                
+                paymentSystem.ProcessPayment(order.Total);
+
+                Console.WriteLine("Comanda a fost procesată cu succes!");
+            }
+        }
+    }
+}
---
+++ Examen1/Examen1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Examen1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Introduceți numele clientului: ");
+            string customerName = Console.ReadLine();
+
+            Console.Write("Introduceți data comenzii (yyyy-mm-dd): ");
+            DateTime orderDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Introduceți valoarea totală a comenzii: ");
+            decimal orderTotal = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Introduceți metoda de plată (Card/Cash): ");
+            string paymentMethod = Console.ReadLine();
+        }
+    }
